aec0575be559c240e9926b76821b0bcb0b5464d5
RiccardoMacchi <rick.mac97@outlook.com>
2024-07-04 15:36:04 +0200

    rimosso hello word aggiunto components AppTitle

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+
+import AppTitle from './components/AppTitle.vue'
+export default{
+  components: {
+    AppTitle
+  }
+}
 </script>
 
 <template>
   <div>
+    <AppTitle/>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,7 +18,6 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
